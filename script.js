f7e7d143fec68907ccc5d27c735a6622c3fceab3
fawadashraf1262 <fawadashraf1262@gmail.com>
2023-10-02 11:50:48 +0500

    completed

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-fetch(`http://www.omdbapi.com/?s=batman&apikey=e0414938`)
-  .then((res) => res.json())
-  .then((data) => {
-    console.log(data);
-  })
+function clickM() {
+  const Title = document.getElementById("title").value;
+  const Year = document.getElementById("year").value;
+  fetch(`http://www.omdbapi.com/?s=${Title}&y=${Year}&apikey=e0414938`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      data.Search.map((data) => {
+        const dv = document.createElement("div");
+        dv.innerHTML = `
+       <h2 id="title">${data.Title}</h2>
+        <p id="year">${data.Year}</p>
+        <img src="${data.Poster}" alt="" id="img" />
+      `;
 
-  .catch((error) => {
-    console.error("An error occurred:", error);
-  });
+        const con = document.querySelector(".movie-list");
+        con.appendChild(dv);
+      });
+      // document.getElementById("img").src = data["Search"][1].Poster;
+      // document.getElementById("year").innerHTML = data["Search"][1].Year;
+    })
+
+    .catch((error) => {
+      console.error("An error occurred:", error);
+    });
+}
